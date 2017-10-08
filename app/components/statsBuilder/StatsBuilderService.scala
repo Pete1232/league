49f295cf9356b22a7bfbbc191fdf3e7f28fa843b
Pete1232 <pete1232@gmail.com>
2017-10-08 12:29:53 +0100

    Added some debug logging

--- app/components/statsBuilder/StatsBuilderService.scala
+++ app/components/statsBuilder/StatsBuilderService.scala
@@ -2,11 +2,15 @@ package components.statsBuilder
 
 import components._
 import connectors.getAllChampions.models.ChampionStats
+import play.api.Logger
 
 object StatsBuilderService {
+  val logger = Logger(this.getClass)
 
   def getMaxBaseHp(championStats: ChampionStats): Double = {
     import championStats._
-    hp + hpperlevel * (MAX_LEVEL - 1)
+    val res = hp + hpperlevel * (MAX_LEVEL - 1)
+    logger.debug(s"Calculated max level $res for $championStats")
+    res
   }
 }
--- app/connectors/getAllChampions/GetAllChampionsConnector.scala
+++ app/connectors/getAllChampions/GetAllChampionsConnector.scala
@@ -3,12 +3,14 @@ package connectors.getAllChampions
 import connectors._
 import connectors.getAllChampions.models.Champions
 import connectors.utilities.ConnectorConfig
+import play.api.Logger
 import play.api.libs.ws.WSClient
 
 import scala.concurrent.{ExecutionContext, Future}
 
 class GetAllChampionsConnector(connectorConfig: ConnectorConfig)
                               (implicit wsClient: WSClient, ec: ExecutionContext) {
+  val logger = Logger(this.getClass)
 
   private val requestUrl = {
     import connectorConfig._
@@ -19,12 +21,21 @@ class GetAllChampionsConnector(connectorConfig: ConnectorConfig)
   import io.circe.generic.auto._
   import io.circe.parser._
 
-  def getAllChampions: Future[Either[Error, Champions]] =
-    wsClient.url(requestUrl)
+  def getAllChampions: Future[Either[Error, Champions]] = {
+    val request = wsClient.url(requestUrl)
       .withHttpHeaders(RIOT_TOKEN_HEADER_KEY -> connectorConfig.apiKey)
       .withQueryStringParameters(TAGS.QUERY_KEY -> TAGS.STATS)
-      .get()
+
+    logger.debug(
+      s"""Requesting champions list
+         |<url> : ${request.url}
+         |<headers> : ${request.headers.keys}
+         |<queryParams> : ${request.queryString}
+         |""".stripMargin)
+
+    request.get()
       .map { res =>
         decode[Champions](res.body)
       }
+  }
 }
